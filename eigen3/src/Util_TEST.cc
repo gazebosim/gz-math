616998af6d8e27d181e4c2b151e974d8a392eab7
AmrElsersy <amrelsersay@gmail.com>
2021-08-19 02:34:22 +0200

    Add Test
    Signed-off-by: AmrElsersy <amrelsersay@gmail.com>

--- eigen3/src/Util_TEST.cc
+++ eigen3/src/Util_TEST.cc
@@ -19,7 +19,44 @@
 
 #include <ignition/math/eigen3/Util.hh>
 
+using namespace ignition;
+
 /////////////////////////////////////////////////
-TEST(EigenUtil, MeshToOrientedBox)
+/// \brief Test the oriented box converted from a set of vertices
+TEST(EigenUtil, meshToOrientedBox)
 {
+  std::vector<math::Vector3d> mesh;
+
+  mesh.push_back(math::Vector3d(1, 0, 0.5));
+  mesh.push_back(math::Vector3d(2, 0.1, 0.4));
+  mesh.push_back(math::Vector3d(2, 1, 3));
+  mesh.push_back(math::Vector3d(1.6, 0.3, 0.1));
+  mesh.push_back(math::Vector3d(1.5, 0.5, 1));
+  mesh.push_back(math::Vector3d(1.4, 1, 3));
+  mesh.push_back(math::Vector3d(1, 0.4, 0.7));
+  mesh.push_back(math::Vector3d(0.9, 1.3, 0));
+  mesh.push_back(math::Vector3d(0.6, 4, 2));
+  mesh.push_back(math::Vector3d(0, 3, 3));
+  mesh.push_back(math::Vector3d(-1, -2, 4));
+  mesh.push_back(math::Vector3d(-2, -2, 0.6));
+
+  math::OrientedBoxd box = math::eigen3::meshToOrientedBox(mesh);
+
+  auto position = box.Pose().Pos();
+  auto rotation = box.Pose().Rot();
+  auto size = box.Size();
+
+  double error = 0.1;
+
+  EXPECT_NEAR(size.X(), 3.09, error);
+  EXPECT_NEAR(size.Y(), 4.39, error);
+  EXPECT_NEAR(size.Z(), 6.63, error);
+
+  EXPECT_NEAR(position.X(), 0.38, error);
+  EXPECT_NEAR(position.Y(), 0.47, error);
+  EXPECT_NEAR(position.Z(), 1.96, error);
+
+  EXPECT_NEAR(rotation.Roll(), -1.66, error);
+  EXPECT_NEAR(rotation.Pitch(), 0.4, error);
+  EXPECT_NEAR(rotation.Yaw(), 2.7, error);
 }
