6bcf289f9550371d378b36a051dd9e449e143b96
ahcorde <ahcorde@gmail.com>
2021-12-17 12:45:38 +0100

    Add DiffDriveOdometry pybind11 interface and examples
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- src/python_pybind11/src/_ignition_math_pybind11.cc
+++ src/python_pybind11/src/_ignition_math_pybind11.cc
@@ -14,6 +14,7 @@
 
 #include <pybind11/pybind11.h>
 
+#include "DiffDriveOdometry.hh"
 #include "Vector2.hh"
 #include "Vector3.hh"
 #include "Vector4.hh"
@@ -24,8 +25,8 @@ PYBIND11_MODULE(math, m)
 {
   m.doc() = "Ignition Math Python Library.";
 
-
-
+  ignition::math::python::defineMathDiffDriveOdometry(
+    m, "DiffDriveOdometry");
 
   ignition::math::python::defineMathVector2<double>(m, "Vector2d");
   ignition::math::python::defineMathVector2<int>(m, "Vector2i");
