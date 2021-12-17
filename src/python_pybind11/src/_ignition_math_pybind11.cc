32153aafe204a551a0f9797f0f191a3a99f412c9
ahcorde <ahcorde@gmail.com>
2021-12-17 12:28:38 +0100

    Added Rand pybind11 interface and example
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- src/python_pybind11/src/_ignition_math_pybind11.cc
+++ src/python_pybind11/src/_ignition_math_pybind11.cc
@@ -14,6 +14,7 @@
 
 #include <pybind11/pybind11.h>
 
+#include "Rand.hh"
 #include "Vector2.hh"
 #include "Vector3.hh"
 #include "Vector4.hh"
@@ -24,8 +25,7 @@ PYBIND11_MODULE(math, m)
 {
   m.doc() = "Ignition Math Python Library.";
 
-
-
+  ignition::math::python::defineMathRand(m, "Rand");
 
   ignition::math::python::defineMathVector2<double>(m, "Vector2d");
   ignition::math::python::defineMathVector2<int>(m, "Vector2i");
