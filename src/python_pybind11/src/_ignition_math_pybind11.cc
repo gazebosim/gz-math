13ea87a88a7a0861c9cc4f889dc507b371e20bfd
Alejandro Hernández <ahcorde@gmail.com>
2021-12-24 12:27:28 +0100

    Added SemanticVersion pybind11 interface
    Signed-off-by: Alejandro Hernández <ahcorde@gmail.com>

--- src/python_pybind11/src/_ignition_math_pybind11.cc
+++ src/python_pybind11/src/_ignition_math_pybind11.cc
@@ -23,6 +23,7 @@
 #include "Quaternion.hh"
 #include "Rand.hh"
 #include "RollingMean.hh"
+#include "SemanticVersion.hh"
 #include "StopWatch.hh"
 #include "Vector2.hh"
 #include "Vector3.hh"
@@ -50,6 +51,8 @@ PYBIND11_MODULE(math, m)
   ignition::math::python::defineMathRand(m, "Rand");
 
   ignition::math::python::defineMathRollingMean(m, "RollingMean");
+
+  ignition::math::python::defineMathSemanticVersion(m, "SemanticVersion");
 
   ignition::math::python::defineMathStopwatch(m, "Stopwatch");
 
