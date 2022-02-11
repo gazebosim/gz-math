5db3397548915bc94599617ef7193570a787d740
Steve Peters <scpeters@openrobotics.org>
2022-02-10 23:47:12 -0800

    Unique pybind11 translation units for MassMatrix3
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- src/python_pybind11/src/_ignition_math_pybind11.cc
+++ src/python_pybind11/src/_ignition_math_pybind11.cc
@@ -150,8 +150,7 @@ PYBIND11_MODULE(math, m)
   ignition::math::python::defineMathPose3<double>(m, "Pose3d");
   ignition::math::python::defineMathPose3<float>(m, "Pose3f");
 
-  ignition::math::python::defineMathMassMatrix3<double>(m, "MassMatrix3d");
-  ignition::math::python::defineMathMassMatrix3<float>(m, "MassMatrix3f");
+  ignition::math::python::defineMathMassMatrix3(m, "MassMatrix3");
 
   ignition::math::python::defineMathSphere<double>(m, "Sphered");
 
