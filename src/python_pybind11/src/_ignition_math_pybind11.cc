7b2747e733c4ef83456688d29d02f692ae602b78
Steve Peters <scpeters@openrobotics.org>
2022-02-10 23:42:05 -0800

    Sepatate pybind11 translation units for Matrix4
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- src/python_pybind11/src/_ignition_math_pybind11.cc
+++ src/python_pybind11/src/_ignition_math_pybind11.cc
@@ -130,9 +130,7 @@ PYBIND11_MODULE(math, m)
 
   ignition::math::python::defineMathMatrix3(m, "Matrix3");
 
-  ignition::math::python::defineMathMatrix4<int>(m, "Matrix4i");
-  ignition::math::python::defineMathMatrix4<double>(m, "Matrix4d");
-  ignition::math::python::defineMathMatrix4<float>(m, "Matrix4f");
+  ignition::math::python::defineMathMatrix4(m, "Matrix4");
 
   ignition::math::python::defineMathTriangle<int>(m, "Trianglei");
   ignition::math::python::defineMathTriangle<double>(m, "Triangled");
