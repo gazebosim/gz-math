6d11eccd238b182be5d28e685be734bcf63144d6
Arjo Chakravarty <arjo@openrobotics.org>
2022-04-08 19:42:50 +0800

    Adds a volumetric grid field to help with lookup in 3D.
    The following PR adds a datastructure that represents a sparse volumetric grid. It allows for storing data in 3D.
    
    # TODO
    - [ ] Write unit tests
    - [ ] docs
    
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

---
+++ include/ignition/math/VolumetricGridLookupField.hh
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef IGNITION_MATH_VOLUMETRIC_GRID_LOOKUP_FIELD_HH_
+#define IGNITION_MATH_VOLUMETRIC_GRID_LOOKUP_FIELD_HH_
+
+#include <vector>
+
+#include <ignition/math/Vector3.hh>
+
+#include <ignition/math/detail/AxisIndex.hh>
+
+namespace ignition
+{
+  namespace math
+  {
+    // Inline bracket to help doxygen filtering.
+    inline namespace IGNITION_MATH_VERSION_NAMESPACE {
+      template<typename T>
+      /// \brief Lookup table for a volumetric dataset. This class is used to
+      /// lookup indices for a large dataset thats organized in a grid. This
+      /// class is not meant to be used by non-grid like data sets. The grid
+      /// may be sparse or non uniform and missing data points.
+      class VolumetricGridLookupField
+      {
+        /// Get the index along the given axis
+        private: AxisIndex<T> z_indices_by_depth;
+
+        private: AxisIndex<T> x_indices_by_lat;
+
+        private: AxisIndex<T> y_indices_by_lon;
+
+        private: std::vector<
+          std::vector<std::vector<std::optional<std::size_t>>>> index_table;
+
+        /// \brief Constructor
+        /// \param[in] _cloud The cloud of points to use to construct the grid.
+        public: VolumetricGridLookupField(
+          const std::vector<Vector3<T>> &_cloud)
+        {
+          // NOTE: This part of the code assumes an exact grid of points.
+          // The grid may be distorted or the stride between different points
+          // may not be the same, but fundamentally the data is structured in
+          // may It keeps track of the axis indices for each point in the grid.
+          for(auto pt: _cloud)
+          {
+            x_indices_by_lat.AddIndexIfNotFound(pt.X());
+            y_indices_by_lon.AddIndexIfNotFound(pt.Y());
+            z_indices_by_depth.AddIndexIfNotFound(pt.Z());
+          }
+
+          int num_x = x_indices_by_lat.GetNumUniqueIndices();
+          int num_y = y_indices_by_lon.GetNumUniqueIndices();
+          int num_z = z_indices_by_depth.GetNumUniqueIndices();
+
+          index_table.resize(num_z);
+          for(int i = 0; i < num_z; ++i)
+          {
+            index_table[i].resize(num_y);
+            for(int j = 0; j < num_y; ++j)
+            {
+              index_table[i][j].resize(num_x);
+            }
+          }
+
+          for(std::size_t i = 0; i < _cloud.size(); ++i)
+          {
+            auto pt = _cloud[i];
+            std::size_t x_index = x_indices_by_lat.GetIndex(pt.X()).value();
+            std::size_t y_index = y_indices_by_lon.GetIndex(pt.Y()).value();
+            std::size_t z_index = z_indices_by_depth.GetIndex(pt.Z()).value();
+            index_table[z_index][y_index][x_index] = i;
+          }
+        }
+
+        /// \brief Retrieves the indices of the points that are to be used for
+        /// interpolation.
+        /// \param[in] _pt The point to get the interpolators for.
+        /// \returns A list of points which are to be used for interpolation. If
+        /// the point is in the middle of a grid cell then it returns the four
+        /// corners of the grid cell. If its along a plane then it returns the
+        /// four corners, if along an edge two points and if coincident with a
+        /// corner one point. If the data is sparse and missing indices then a
+        /// nullopt will be returned.
+        public: std::vector<std::optional<std::size_t>>
+          GetInterpolators(Vector3<T> &_pt) const
+        {
+          std::vector<std::optional<std::size_t>> interpolators;
+          /// Provides
+          auto x_indices = x_indices_by_lat.GetInterpolators(_pt.X());
+          auto y_indices = y_indices_by_lon.GetInterpolators(_pt.Y());
+          auto z_indices = z_indices_by_depth.GetInterpolators(_pt.Z());
+
+          /// At the point
+          for(auto x_index: x_indices)
+          {
+            for(auto y_index: y_indices)
+            {
+              for(auto z_index: z_indices)
+              {
+                auto index = index_table[z_index][y_index][x_index];
+                interpolators.push_back(index);
+              }
+            }
+          }
+
+          return interpolators;
+        }
+      };
+    }
+  }
+}
+
+#endif
---
+++ src/VolumetricGridLookupField.cc
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#include <ignition/math/VolumetricGridLookupField.hh>
+
+#include <gtest/gtest.h>
+using namespace ignition;
+using namespace math;
+
+
+TEST(NDArrayLookup, CheckInterpolationExact)
+{
+  // This tests query performance of 54000 points in a 3D grid.
+  std::vector<Vector3d> cloud;
+  const double stride_x = 1, stride_y = 5, stride_z = 10;
+  for(double x = 0; x < 300; x += stride_x)
+  {
+    for(double y = 0; y < 300; y += stride_y)
+    {
+      for(double z = 0; z < 300; z += stride_z)
+      {
+        cloud.emplace_back(x, y, z);
+      }
+    }
+  }
+  VolumetricGridLookupField<double> scalarIndex(cloud);
+
+  for(std::size_t i = 0; i < cloud.size(); ++i)
+  {
+    auto val = scalarIndex.GetInterpolators(cloud[i]);
+    ASSERT_EQ(val.size(), 1);
+    ASSERT_EQ(val[0], i);
+  }
+}
+
+TEST(NDArrayLookup, CheckInterpolationBoxEightPoints)
+{
+  //pcl::PointCloud<pcl::PointXYZ> cloud;
+  //cloud.push_back(pcl::PointXYZ(0, 0, 0));
+  //cloud.push_back(pcl::PointXYZ(0, 0, 1));
+  //cloud.push_back(pcl::PointXYZ(0, 1, 0));
+  //cloud.push_back(pcl::PointXYZ(0, 1, 1));
+  //cloud.push_back(pcl::PointXYZ(1, 0, 0));
+  //cloud.push_back(pcl::PointXYZ(1, 0, 1));
+  //cloud.push_back(pcl::PointXYZ(1, 1, 0));
+  //cloud.push_back(pcl::PointXYZ(1, 1, 1));
+  /*for(double x = 0; x < 300; x += stride_x)
+  {
+    for(double y = 0; y < 300; y += stride_y)
+    {
+      for(double z = 0; z < 300; z += stride_z)
+      {
+        cloud.push_back(pcl::PointXYZ(x, y, z));
+      }
+    }
+  }
+
+  VolumetricScalarField scalarIndex(cloud);
+
+  for(std::size_t i = 0; i < cloud.size(); ++i)
+  {
+    auto val = scalarIndex.GetInterpolators(cloud[i]);
+    ASSERT_EQ(val.size(), 1);
+    ASSERT_EQ(val[0], i);
+  }*/
+}
