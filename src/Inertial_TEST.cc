850888916e7ab56d31fbd2f97cb71d42fc63fcb5
deepanshu <deepanshubansal01@gmail.com>
2022-05-25 18:38:58 -0400

    add -operator unit test along with +operator
    Signed-off-by: deepanshu <deepanshubansal01@gmail.com>

--- src/Inertial_TEST.cc
+++ src/Inertial_TEST.cc
@@ -366,9 +366,10 @@ TEST(Inertiald_Test, Diagonalize)
                       0.25*math::Vector3d(-sqrt(3), -sqrt(3)/2, 3.0));
 }
 /////////////////////////////////////////////////
-TEST(Inertiald_Test, Addition)
+TEST(Inertiald_Test, AdditionSubtraction)
 {
-  // Add two half-cubes together
+  // Add two half-cubes together and
+  // Subtract one half-cube from a full cube
   {
     const double mass = 12.0;
     const math::Vector3d size(1, 1, 1);
@@ -376,11 +377,15 @@ TEST(Inertiald_Test, Addition)
     EXPECT_TRUE(cubeMM3.SetFromBox(mass, size));
     const math::Inertiald cube(cubeMM3, math::Pose3d::Zero);
     math::MassMatrix3d half;
-    EXPECT_TRUE(half.SetFromBox(0.5*mass, math::Vector3d(0.5, 1, 1)));
+    const math::Vector3d half_size(0.5, 1, 1);
+    EXPECT_TRUE(half.SetFromBox(0.5*mass, half_size));
     math::Inertiald left(half, math::Pose3d(-0.25, 0, 0, 0, 0, 0));
     math::Inertiald right(half, math::Pose3d(0.25, 0, 0, 0, 0, 0));
     EXPECT_EQ(cube, left + right);
     EXPECT_EQ(cube, right + left);
+    EXPECT_EQ(right, cube - left);
+    EXPECT_EQ(left, cube - right);
+
     // test += operator
     {
       math::Inertiald tmp = left;
@@ -392,6 +397,18 @@ TEST(Inertiald_Test, Addition)
       tmp += left;
       EXPECT_EQ(cube, tmp);
     }
+    // test -= operator
+    {
+      math::Inertiald tmp = cube;
+      tmp -= right;
+      EXPECT_EQ(left, tmp);
+    }
+    {
+      math::Inertiald tmp = right;
+      tmp -= left;
+      EXPECT_EQ(right, tmp);
+    }
+
     // Test EquivalentBox
     {
       math::Vector3d size2;
@@ -407,9 +424,24 @@ TEST(Inertiald_Test, Addition)
       EXPECT_EQ(size, size2);
       EXPECT_EQ(rot2, math::Quaterniond::Identity);
     }
+    {
+      math::Vector3d size2;
+      math::Quaterniond rot2;
+      EXPECT_TRUE((cube - right).MassMatrix().EquivalentBox(size2, rot2));
+      EXPECT_EQ(half_size, size2);
+      EXPECT_EQ(rot2, math::Quaterniond::Identity);
+    }
+    {
+      math::Vector3d size2;
+      math::Quaterniond rot2;
+      EXPECT_TRUE((cube - left).MassMatrix().EquivalentBox(size2, rot2));
+      EXPECT_EQ(half_size, size2);
+      EXPECT_EQ(rot2, math::Quaterniond::Identity);
+    }
   }
 
-  // Add two rotated half-cubes together
+  // Add two rotated half-cubes together and
+  // Subtract a rotated half-cube from rotated full-cube
   {
     const double mass = 12.0;
     const math::Vector3d size(1, 1, 1);
@@ -425,6 +457,7 @@ TEST(Inertiald_Test, Addition)
     // objects won't match exactly
     // since inertia matrices will all be in base frame
     // but mass, center of mass, and base-frame MOI should match
+    // +operator
     EXPECT_NE(cube, left + right);
     EXPECT_NE(cube, right + left);
     EXPECT_DOUBLE_EQ(cubeMM3.Mass(), (left + right).MassMatrix().Mass());
@@ -433,6 +466,15 @@ TEST(Inertiald_Test, Addition)
     EXPECT_EQ(cube.Pose().Pos(), (right + left).Pose().Pos());
     EXPECT_EQ(cube.Moi(), (left + right).Moi());
     EXPECT_EQ(cube.Moi(), (right + left).Moi());
+    // -operator
+    EXPECT_NE(left, cube - right);
+    EXPECT_NE(right, cube - left);
+    EXPECT_DOUBLE_EQ(left.MassMatrix().Mass(), (cube - right).MassMatrix().Mass());
+    EXPECT_DOUBLE_EQ(right.MassMatrix().Mass(), (cube - left).MassMatrix().Mass());
+    EXPECT_EQ(left.Pose().Pos(), (cube - right).Pose().Pos());
+    EXPECT_EQ(right.Pose().Pos(), (cube - left).Pose().Pos());
+    EXPECT_EQ(left.Moi(), (cube - right).Moi());
+    EXPECT_EQ(right.Moi(), (cube - left).Moi());
   }
 
   // Add eight cubes together into larger cube
@@ -601,84 +643,5 @@ TEST(Inertiald_Test, AdditionInvalid)
     EXPECT_TRUE((i0 + i).MassMatrix().IsPositive());
     EXPECT_TRUE((i + i0).MassMatrix().IsValid());
     EXPECT_TRUE((i0 + i).MassMatrix().IsValid());
-  }
-}
-
-/////////////////////////////////////////////////
-TEST(Inertiald_Test, Subtraction){
-
-  // Subtract one half-cube from a full cube
-  { 
-    // full cube
-    const double mass = 12.0;
-    const math::Vector3d size(1, 1, 1);
-    math::MassMatrix3d cubeMM3;
-    EXPECT_TRUE(cubeMM3.SetFromBox(mass, size));
-    const math::Inertiald cube(cubeMM3, math::Pose3d::Zero);
-
-    // two half cubes
-    math::MassMatrix3d half;
-    const math::Vector3d half_size(0.5, 1, 1);
-    EXPECT_TRUE(half.SetFromBox(0.5*mass, half_size));
-    math::Inertiald left(half, math::Pose3d(-0.25, 0, 0, 0, 0, 0));
-    math::Inertiald right(half, math::Pose3d(0.25, 0, 0, 0, 0, 0));
-
-    EXPECT_EQ(right, cube - left);
-    EXPECT_EQ(left, cube - right);
-    // test -= operator
-    {
-      math::Inertiald tmp = cube;
-      tmp -= right;
-      EXPECT_EQ(left, tmp);
-    }
-    {
-      math::Inertiald tmp = right;
-      tmp -= left;
-      EXPECT_EQ(right, tmp);
-    }
-     // Test EquivalentBox
-    {
-      math::Vector3d size2;
-      math::Quaterniond rot2;
-      EXPECT_TRUE((cube - right).MassMatrix().EquivalentBox(size2, rot2));
-      EXPECT_EQ(half_size, size2);
-      EXPECT_EQ(rot2, math::Quaterniond::Identity);
-    }
-    {
-      math::Vector3d size2;
-      math::Quaterniond rot2;
-      EXPECT_TRUE((cube - left).MassMatrix().EquivalentBox(size2, rot2));
-      EXPECT_EQ(half_size, size2);
-      EXPECT_EQ(rot2, math::Quaterniond::Identity);
-    }
-  }
-
-  // Subtract a rotated half-cube from rotated full-cube
-  {
-    // rotated full-cube
-    const double mass = 12.0;
-    const math::Vector3d size(1, 1, 1);
-    math::MassMatrix3d cubeMM3;
-    EXPECT_TRUE(cubeMM3.SetFromBox(mass, size));
-    const math::Inertiald cube(cubeMM3, math::Pose3d(0, 0, 0, IGN_PI_4, 0, 0));
-
-    // two  rotated half-cubes
-    math::MassMatrix3d half;
-    const math::Vector3d half_size(0.5, 1, 1);
-    EXPECT_TRUE(half.SetFromBox(0.5*mass, half_size));
-    math::Inertiald left(half, math::Pose3d(-0.25, 0, 0, IGN_PI_4, 0, 0));
-    math::Inertiald right(half, math::Pose3d(0.25, 0, 0, IGN_PI_4, 0, 0));
-
-    // objects won't match exactly
-    // since inertia matrices will all be in base frame
-    // but mass, center of mass, and base-frame MOI should match
-    EXPECT_NE(left, cube - right);
-    EXPECT_NE(right, cube - left);
-    EXPECT_DOUBLE_EQ(left.MassMatrix().Mass(), (cube - right).MassMatrix().Mass());
-    EXPECT_DOUBLE_EQ(right.MassMatrix().Mass(), (cube - left).MassMatrix().Mass());
-    EXPECT_EQ(left.Pose().Pos(), (cube - right).Pose().Pos());
-    EXPECT_EQ(right.Pose().Pos(), (cube - left).Pose().Pos());
-    EXPECT_EQ(left.Moi(), (cube - right).Moi());
-    EXPECT_EQ(right.Moi(), (cube - left).Moi());
   }
 }
