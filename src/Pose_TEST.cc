df139027f464fb836659830fe5cff424cd073573
Louise Poubel <louise@openrobotics.org>
2022-06-08 21:00:17 -0700

    new IGN_PI
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- src/Pose_TEST.cc
+++ src/Pose_TEST.cc
@@ -91,7 +91,7 @@ TEST(PoseTest, Pose)
     // B is the transform from O to Q in frame O
     // then -A is transform from P to O specified in frame P
     math::Pose3d A(math::Vector3d(1, 0, 0),
-        math::Quaterniond(0, 0, IGN_PI/4.0));
+        math::Quaterniond(0, 0, GZ_PI/4.0));
     EXPECT_TRUE(math::equal(
         (A.Inverse() * math::Pose3d()).Pos().X(),      -1.0/sqrt(2)));
     EXPECT_TRUE(math::equal(
@@ -103,7 +103,7 @@ TEST(PoseTest, Pose)
     EXPECT_TRUE(math::equal(
         (A.Inverse() * math::Pose3d()).Rot().Euler().Y(),  0.0));
     EXPECT_TRUE(math::equal(
-        (A.Inverse() * math::Pose3d()).Rot().Euler().Z(), -IGN_PI/4));
+        (A.Inverse() * math::Pose3d()).Rot().Euler().Z(), -GZ_PI/4));
 
     GZ_UTILS_WARN_IGNORE__DEPRECATED_DECLARATION
     // Coverage for unitary - operator
@@ -113,7 +113,7 @@ TEST(PoseTest, Pose)
     EXPECT_TRUE(math::equal((-A).Pos().Z(),               0.0));
     EXPECT_TRUE(math::equal((-A).Rot().Euler().X(),  0.0));
     EXPECT_TRUE(math::equal((-A).Rot().Euler().Y(),  0.0));
-    EXPECT_TRUE(math::equal((-A).Rot().Euler().Z(), -IGN_PI/4.0));
+    EXPECT_TRUE(math::equal((-A).Rot().Euler().Z(), -GZ_PI/4.0));
     GZ_UTILS_WARN_RESUME__DEPRECATED_DECLARATION
   }
   {
@@ -124,13 +124,13 @@ TEST(PoseTest, Pose)
     math::Pose3d A(math::Vector3d(1, 0, 0),
         math::Quaterniond(0, 0, GZ_PI/4.0));
     math::Pose3d B(math::Vector3d(1, 1, 0),
-        math::Quaterniond(0, 0, IGN_PI/2.0));
+        math::Quaterniond(0, 0, GZ_PI/2.0));
     EXPECT_TRUE(math::equal((A.Inverse() * B).Pos().X(),       1.0/sqrt(2)));
     EXPECT_TRUE(math::equal((A.Inverse() * B).Pos().Y(),       1.0/sqrt(2)));
     EXPECT_TRUE(math::equal((A.Inverse() * B).Pos().Z(),               0.0));
     EXPECT_TRUE(math::equal((A.Inverse() * B).Rot().Euler().X(),  0.0));
     EXPECT_TRUE(math::equal((A.Inverse() * B).Rot().Euler().Y(),  0.0));
-    EXPECT_TRUE(math::equal((A.Inverse() * B).Rot().Euler().Z(), IGN_PI/4.0));
+    EXPECT_TRUE(math::equal((A.Inverse() * B).Rot().Euler().Z(), GZ_PI/4.0));
 
     GZ_UTILS_WARN_IGNORE__DEPRECATED_DECLARATION
     // Coverage for - operator
