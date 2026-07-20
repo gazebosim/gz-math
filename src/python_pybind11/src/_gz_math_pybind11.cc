154e62a4c9f7e33ede031ffe53a774492d4cef23
Gokul <43350089+gokulp01@users.noreply.github.com>
2026-07-20 06:32:09 -0700

    Add pybindings for VolumetricGridLookupField (#657)
    Signed-off-by: gokulp01 <gokulgns@gmail.com>
    Signed-off-by: Gokul <43350089+gokulp01@users.noreply.github.com>
    Signed-off-by: Carlos Aguero <caguero@honurobotics.com>
    (cherry picked from commit 12f4a7bb1900212cae041ed69eff3493540c5856)
    
    # Conflicts:
    #	src/python_pybind11/src/_gz_math_pybind11.cc

--- src/python_pybind11/src/_gz_math_pybind11.cc
+++ src/python_pybind11/src/_gz_math_pybind11.cc
@@ -28,6 +28,7 @@
 #include "GaussMarkovProcess.hh"
 #include "Helpers.hh"
 #include "Inertial.hh"
+#include "InterpolationPoint.hh"
 #include "Interval.hh"
 #include "Kmeans.hh"
 #include "Line2.hh"
@@ -62,6 +63,11 @@
 #include "Vector3.hh"
 #include "Vector3Stats.hh"
 #include "Vector4.hh"
+<<<<<<< HEAD
+=======
+#include "VolumetricGridLookupField.hh"
+#include "OccupancyGrid.hh"
+>>>>>>> 12f4a7b (Add pybindings for VolumetricGridLookupField (#657))
 
 namespace py = pybind11;
 
@@ -190,4 +196,16 @@ PYBIND11_MODULE(BINDINGS_MODULE_NAME, m)
     m, "OnePoleQuaternion");
   gz::math::python::defineMathOnePoleVector3(
     m, "OnePoleVector3");
+<<<<<<< HEAD
+=======
+
+  gz::math::python::defineMathCoordinateVector3(m, "CoordinateVector3");
+
+  gz::math::python::defineMathOccupancyGrid(m, "OccupancyGrid");
+
+  gz::math::python::defineMathVolumetricGridLookupField(
+    m, "VolumetricGridLookupField");
+
+  gz::math::python::defineMathInterpolationPoint3D(m, "InterpolationPoint3D");
+>>>>>>> 12f4a7b (Add pybindings for VolumetricGridLookupField (#657))
 }
